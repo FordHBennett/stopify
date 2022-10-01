6929d58f445a49ce7ed5eecd6f2a593671232e1b
kqwang123 <32421263+kqwang123@users.noreply.github.com>
2022-10-01 18:43:27 -0500

    got users!

--- back-end/authorization_code/app.js
+++ back-end/authorization_code/app.js
@@ -73,8 +73,21 @@ app.get("/callback", async (req, res) => {
     }
   }) 
   const data = await response.json();
-  console.log(data);
+  global.access_token = data.access_token;
+  res.redirect("/dashboard");
 });
+
+app.get("/dashboard", async (req, res) => {
+  const response = await fetch("https://api.spotify.com/v1/me", {
+    method: "get",
+    headers: {
+      Authorization: "Bearer " + global.access_token,
+    }
+  })
+  const data = await response.json();
+  console.log(data);
+  //res.render("dashboard");
+})
 
 console.log('Listening on 8888');
 app.listen(8888);
