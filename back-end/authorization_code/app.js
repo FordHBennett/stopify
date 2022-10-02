b0cdabadd7a1a9cf6b991d2ad635cd15e4fe3570
Ford Hideo Bennett <ford660bennett@tamu.edu>
2022-10-02 04:10:03 -0500

    up

--- back-end/authorization_code/app.js
+++ back-end/authorization_code/app.js
@@ -100,18 +100,17 @@ app.get("/dashboard", async (req, res) => {
 
 console.log('Listening on netlify');
 app.listen("63394b7be466922871834a3e--stoopify.netlify.app");
-=======
  var express = require('express'); // Express web server framework
  var fetch = require('node-fetch');
  var cors = require('cors');
  var querystring = require('querystring');
  var cookieParser = require('cookie-parser');
  const { response } = require('express');
- 
+
  var client_id = 'd0b9b3b01a4c4977bb99be70d13c8e24'; // Your client id
  var client_secret = 'a79fa79340184ca6aebec28e555366f3'; // Your secret
  var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
- 
+
  /**
   * Generates a random string containing numbers and letters
   * @param  {number} length The length of the string
@@ -120,21 +119,21 @@ app.listen("63394b7be466922871834a3e--stoopify.netlify.app");
  var generateRandomString = function(length) {
    var text = '';
    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
- 
+
    for (var i = 0; i < length; i++) {
      text += possible.charAt(Math.floor(Math.random() * possible.length));
    }
    return text;
  };
- 
+
  var stateKey = 'spotify_auth_state';
- 
+
  var app = express();
- 
+
  app.use(express.static(__dirname + '/public'))
     .use(cors())
     .use(cookieParser());
- 
+
  app.get('/login', function(req, res) {
    var auth_query_parameters = new URLSearchParams({
      response_type: 'code',
@@ -144,12 +143,12 @@ app.listen("63394b7be466922871834a3e--stoopify.netlify.app");
    })
    res.redirect('https://accounts.spotify.com/authorize?' + auth_query_parameters.toString());
  })
- 
+
  app.get("/callback", async (req, res) => {
- 
+
    // your application requests refresh and access tokens
    // after checking the state parameter'
- 
+
    const code = req.query.code;
    var body = new URLSearchParams({
      code: code,
@@ -169,7 +168,7 @@ app.listen("63394b7be466922871834a3e--stoopify.netlify.app");
    global.access_token = data.access_token;
    res.redirect("/dashboard");
  });
- 
+
  async function getData(endpoint) {
    const response = await fetch("https://api.spotify.com/v1" + endpoint, {
      method: "get",
@@ -180,7 +179,7 @@ app.listen("63394b7be466922871834a3e--stoopify.netlify.app");
    const data = await response.json();
    return data;
  }
- 
+
  app.get("/dashboard", async (req, res) => {
    const playlists = await getData("/users/pauljwbae/playlists");
    const items = playlists.items;
@@ -189,6 +188,6 @@ app.listen("63394b7be466922871834a3e--stoopify.netlify.app");
    const tracks = await getData(link);
    console.log(tracks);
  })
- 
+
  console.log('Listening on 8888');
  app.listen(8888);
